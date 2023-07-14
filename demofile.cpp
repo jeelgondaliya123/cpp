702f6921d9ea141ca613d18b9fa7256b5b48546f
jay kalathiya <jaykalathiya251251@gmail.com>
2023-07-14 18:18:04 +0530

    c++ language

---
+++ demofile.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int x,y;
+   cout<<"enter x:";
+   cin>>x;
+   cout<<"enter y:";
+   cin>>y;
+   x=x+y;
+   y=x-y;
+   x=x-y;
+   cout<<"x:"<<x;
+   cout<<"\ny:"<<y;
+}   
+   
